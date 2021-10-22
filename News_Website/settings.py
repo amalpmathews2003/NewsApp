8d3f501146385c580df07917aec207342c398405
Amal P Mathews <amalpmathews2003@gmail.com>
2021-10-22 11:33:45 +0530

    ''

--- News_Website/settings.py
+++ News_Website/settings.py
@@ -84,7 +84,7 @@ ROOT_URLCONF = 'News_Website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
